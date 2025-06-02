233f0d5584636a1370932947e89c81abdc1dc3bb
pedro silva <pedrohcsilva77@gmail.com>
2025-06-02 20:13:07 -0300

    Editando o conteúdo da tela 'Sobre nós'

--- src/pages/Sobre/Sobre.js
+++ src/pages/Sobre/Sobre.js
@@ -9,10 +9,9 @@ function Sobre() {
         <div className="content-sobre">
             <Faixa/>
             <Navbar/>
-            <h1>Bem-vindo à <strong>BookHub</strong>, sua livraria online!</h1>
+            <h1 style={{ marginLeft: '-72px', textAlign: 'start' }}>Sobre a <strong>BookHub</strong></h1>
             <p>
-                A BookHub é um projeto acadêmico desenvolvido por estudantes do curso de <strong>DSM da Fatec Franca</strong>.
-                Nosso objetivo é criar uma plataforma de comércio eletrônico que ofereça uma experiência de compra de livros única e agradável.
+                A BookHub é uma plataforma de livraria online dedicada a proporcionar uma experiência completa, segura e moderna para quem busca os melhores livros do mercado. Nosso compromisso é oferecer um ambiente digital intuitivo, com navegação facilitada, recomendações personalizadas e um catálogo diversificado que contempla desde grandes clássicos da literatura até os lançamentos mais recentes. Trabalhamos para garantir praticidade, segurança e excelência em cada etapa da sua experiência de compra, valorizando o prazer da leitura e incentivando o acesso à cultura e ao conhecimento.<br/>Este site é um projeto acadêmico desenvolvido por estudantes do curso de <strong>Desenvolvimento de Software Multiplataforma (DSM)</strong> da Fatec Franca, como parte de nossa formação profissional.<br/><br/>Equipe responsável: Ana Julia, Gabriel, Lauane, Miguel e Pedro.
             </p>
             <Footer/>
         </div>
